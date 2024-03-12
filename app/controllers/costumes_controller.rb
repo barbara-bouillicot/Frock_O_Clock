7f7fae52451f5661bb0c9f20ed21677d19f132c7
Matthew Gregory <matthew.e.gregory@gmail.com>
2024-03-12 16:12:01 +0000

    most costume controller actions and devise sign up

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -1,5 +1,31 @@
 class CostumesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   def index
     @costumes = Costume.all
+  end
+
+  def new
+    @costume = Costume.new
+  end
+
+  def create
+    @costume = Costume.new(costume_params)
+    @costume.user = current_user
+    if @costume.save
+      redirect_to costume_path(@costume)
+    else
+      raise
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @costume = Costume.find(params[:id])
+  end
+
+  private
+
+  def costume_params
+    params.require(:costume).permit(:name, :category, :size, :material, :description, :price, :user_id)
   end
 end
