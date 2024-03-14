a71375a36ddcaf106b0335342bedd607a7badec8
Luisa Aslanidis lue.aslan@gmail.com <lue.aslan@gmail.com>
2024-03-14 16:36:48 +0100

    Added the Mapbox and autocomplet for location

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   has_many :costumes
   has_many :bookings
   # Include default devise modules. Others available are:
@@ -8,4 +11,5 @@ class User < ApplicationRecord
 
   validates :first_name, :last_name, :password, :location, presence: true
   validates :email, presence: true
+
 end
